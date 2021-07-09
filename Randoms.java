2682247dc79c1ff41b913e4040c3bc73f598777e
wolf30682 <80920822+wolf30682@users.noreply.github.com>
2021-07-09 09:13:26 +0300

    First commit

---
+++ Randoms.java
@@ -0,0 +1,38 @@
+package builder_patterns3;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new iteratorRandom();
+    }
+
+    class iteratorRandom implements Iterator<Integer> {
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(max - min + 1) + min;
+        }
+
+        @Override
+        public void remove() {
+        }
+    }
+}
